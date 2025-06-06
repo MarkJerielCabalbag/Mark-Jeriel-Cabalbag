7ff42b786f34d207b1db26491cdd049257bd1848
MarkJerielCabalbag <cabalbagmarkjeriel03@gmail.com>
2025-06-06 19:19:44 +0800

    edit viteconfig

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Mark-Jeriel-Cabalbag/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
